18663019408ddb2037be1cc5ec26744c356ca90b
Larry Johnson <johnson.larry.l@fintorian.com>
2023-11-11 10:37:22 -0600

    #21 - Grayscale conversion - Sima's code

--- tests/test_color_spaces.py
+++ tests/test_color_spaces.py
@@ -4,7 +4,9 @@ import numpy as np
 from random import choice
 
 from traffic_light_color_detection.color_spaces import cmy_to_rgb, \
+                                                       gray_to_rgb, \
                                                        rgb_to_cmy, \
+                                                       rgb_to_gray, \
                                                        rgb_to_yuv, \
                                                        rgb_to_ycbcr, \
                                                        ycbcr_to_rgb, \
@@ -56,12 +58,28 @@ def test_convert_rgb_to_cmy() -> None:
 
     cmy_image = rgb_to_cmy(original_image)
     cmy_to_rgb_image = cmy_to_rgb(cmy_image)
-    # cmy_to_rgb_cv_image = cv.cvtColor(original_image, cv.COLOR_BGR2YCrCb)
-    # pics = [original_image, cmy_image, cmy_to_rgb_cv_image, cmy_to_rgb_image]
-    # pic_titles = ["Original Image", "RGB-> CMY", "(CV2) RGB -> CMY", "YCbCr->RGB"]
-    # viz_layer(pics, pic_titles)
+
+    # TODO - How does CV2 convert to CMY color space???
+    pics = [original_image, cmy_image, cmy_to_rgb_image]
+    pic_titles = ["Original Image", "RGB-> CMY", "YCbCr->RGB"]
+    viz_layer(pics, pic_titles)
 
     image_comparison = np.isclose(original_image, cmy_to_rgb_image, rtol=2)
+    assert len(np.where(image_comparison == False)) < 5 # Set threshold of the observation of 5 feature vectors not being equal
+
+
+def test_convert_rgb_to_grey() -> None:
+    image_file_path = get_image()
+    original_image = cv.imread(os.path.join(BASE_IMAGE_DIR, image_file_path))
+
+    cmy_image = rgb_to_gray(original_image)
+    gray_to_rgb_image = gray_to_rgb(cmy_image, original_image)
+    gray_to_rgb_cv_image = cv.cvtColor(original_image, cv.COLOR_BGR2GRAY)
+    pics = [original_image, cmy_image, gray_to_rgb_cv_image, gray_to_rgb_image]
+    pic_titles = ["Original Image", "RGB-> GRAY", "(CV2) RGB -> CMY", "GRAY->RGB"]
+    viz_layer(pics, pic_titles)
+
+    image_comparison = np.isclose(original_image, gray_to_rgb_image, rtol=2)
     assert len(np.where(image_comparison == False)) < 5 # Set threshold of the observation of 5 feature vectors not being equal
 
 
--- traffic_light_color_detection/color_spaces.py
+++ traffic_light_color_detection/color_spaces.py
@@ -53,3 +53,19 @@ def rgb_to_cmy(image: np.ndarray) -> np.ndarray:
 def cmy_to_rgb(image: np.ndarray) -> np.ndarray:
     rgb_image = 255 - image
     return rgb_image
+
+
+def rgb_to_gray(image: np.ndarray) -> np.ndarray:
+    gray_channel = 0.21 * image[:, :, 2] + 0.71 * image[:, :, 1] + 0.08 * image[:, :, 0]
+    return gray_channel.astype(np.uint8)
+
+
+def gray_to_rgb(grayscale_image: np.ndarray, original_image: np.ndarray) -> np.ndarray:
+    height, width = grayscale_image.shape[:2]
+    color_image = np.zeros((height, width, 3), dtype=np.uint8)
+
+    for i in range(height):
+        for j in range(width):
+            color_image[i, j] = [original_image[i, j, 0], original_image[i, j, 1], original_image[i, j, 2]]
+
+    return color_image
