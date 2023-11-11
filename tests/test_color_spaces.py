7c8aaa554fada3a1affe07f19dcfc83437996490
Larry Johnson <johnson.larry.l@fintorian.com>
2023-11-11 10:16:38 -0600

    #19 - CMY conversion - Sima's Code

--- tests/test_color_spaces.py
+++ tests/test_color_spaces.py
@@ -3,7 +3,12 @@ import os
 import numpy as np
 from random import choice
 
-from traffic_light_color_detection.color_spaces import rgb_to_yuv, rgb_to_ycbcr, ycbcr_to_rgb, yuv_to_rgb
+from traffic_light_color_detection.color_spaces import cmy_to_rgb, \
+                                                       rgb_to_cmy, \
+                                                       rgb_to_yuv, \
+                                                       rgb_to_ycbcr, \
+                                                       ycbcr_to_rgb, \
+                                                       yuv_to_rgb
 from traffic_light_color_detection.viz import viz_layer
 
 BASE_IMAGE_DIR = os.path.join(os.path.dirname(__file__), "./data/kaggle_dataset/test_dataset/test_images/")
@@ -45,5 +50,18 @@ def test_convert_rgb_to_ycbcr() -> None:
     assert len(np.where(image_comparison == False)) < 5 # Set threshold of the observation of 5 feature vectors not being equal
 
 
+def test_convert_rgb_to_cmy() -> None:
+    image_file_path = get_image()
+    original_image = cv.imread(os.path.join(BASE_IMAGE_DIR, image_file_path))
+
+    cmy_image = rgb_to_cmy(original_image)
+    cmy_to_rgb_image = cmy_to_rgb(cmy_image)
+    # cmy_to_rgb_cv_image = cv.cvtColor(original_image, cv.COLOR_BGR2YCrCb)
+    # pics = [original_image, cmy_image, cmy_to_rgb_cv_image, cmy_to_rgb_image]
+    # pic_titles = ["Original Image", "RGB-> CMY", "(CV2) RGB -> CMY", "YCbCr->RGB"]
+    # viz_layer(pics, pic_titles)
+
+    image_comparison = np.isclose(original_image, cmy_to_rgb_image, rtol=2)
+    assert len(np.where(image_comparison == False)) < 5 # Set threshold of the observation of 5 feature vectors not being equal
 
 
